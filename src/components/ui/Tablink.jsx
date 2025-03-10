4df4bd0838d8663c3eec29e3680937116a7b7a19
TaiyabaBhatti <taiyababhattii@gmail.com>
2025-03-10 20:15:47 +0500

    utilizing links functinality by using navlink

--- src/components/ui/Tablink.jsx
+++ src/components/ui/Tablink.jsx
@@ -3,19 +3,10 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function Tablink({ text, path }) {
   return (
-    <NavLink to={path} className=" font-normal
-    relative
-    before:absolute
-    before:h-0.5 before:w-0
-  hover:before:w-full
-  hover:before:transition-all
-  hover:before:duration-300
+    <NavLink to={path}
     
-    before:left-0
-    before:-bottom-1
-    before:bg-black
-    
-    ">
+    className={({isActive})=> `relative font-normal before:absolute before:h-0.5 before:w-0 hover:before:w-full hover:before:transition-all hover:before:duration-300 before:left-0 before:-bottom-1 before:bg-black ${isActive?"before:w-full" : ""}`}
+    >
       {text}
     </NavLink>
   );
