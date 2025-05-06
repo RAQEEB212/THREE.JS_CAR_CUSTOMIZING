61e71bff400427e3b599f357116d47af83753361
RAQEEB212 <saneelmemon45@gmail.com>
2025-05-06 23:42:07 +0500

    car customizing three.js project

--- src/routing/WebRoutes.jsx
+++ src/routing/WebRoutes.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Hero from '../pages/Home/HeroSection'
 import HomePage from '../pages/Home/HomePage'
 import ContactPage from '../pages/Contact/ContactPage'
 import AboutPage from '../pages/About/AboutPage'
@@ -10,11 +9,16 @@ import AboutPage from '../pages/About/AboutPage'
 
 export default function WebRoutes() {
   return (
- <Routes>
-  <Route path='/' element={<HomePage/>}/>
-  <Route path='/home' element={<HomePage/>}/>
-  <Route path='/about' element={<AboutPage/>}/>
-  <Route path='/contact' element={<ContactPage/>}/>
- </Routes>
+      <>
+          <h1>Welcome to Car-Banao</h1>
+
+          <Routes>
+              <Route path="/" element={<HomePage/>}/>
+              <Route path='/home' element={<HomePage/>}/>
+              <Route path='/about' element={<AboutPage/>}/>
+              <Route path='/contact' element={<ContactPage/>}/>
+          </Routes>
+      </>
+
   )
 }
